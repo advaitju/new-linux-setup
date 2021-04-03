c8cc81b09379c3c6a3c6ff37b5f0b4fe3315c3d8
Advait Junnarkar <advait.ju@gmail.com>
2021-04-03 16:08:18 +1100

    Initial commit

---
+++ init.vim
@@ -0,0 +1,77 @@
+" File moved to ~/.config/nvim/init.vim
+" Uncomment if you have an existing .vimrc
+" set runtimepath^=~/.vim runtimepath+=~/.vim/after
+" let &packpath = &runtimepath
+" source ~/.vimrc
+
+" Enable vim paste mode when pasting to avoid auto-indentation.
+let &t_SI .= "\<Esc>[?2004h"
+let &t_EI .= "\<Esc>[?2004l"
+inoremap <special> <expr> <Esc>[200~ XTermPasteBegin()
+function! XTermPasteBegin()
+  set pastetoggle=<Esc>[201~
+  set paste
+  return ""
+endfunction
+
+" dein plugin manager
+if &compatible
+  set nocompatible
+endif
+" Add the dein installation directory into runtimepath
+set runtimepath+=~/.cache/dein/repos/github.com/Shougo/dein.vim
+
+if dein#load_state('~/.cache/dein')
+  call dein#begin('~/.cache/dein')
+
+  call dein#add('~/.cache/dein/repos/github.com/Shougo/dein.vim')
+  call dein#add('Shougo/deoplete.nvim')
+  
+  " My plugins
+  call dein#add('tpope/vim-fugitive')
+  call dein#add('vim-airline/vim-airline')
+  call dein#add('vim-airline/vim-airline-themes')
+
+  if !has('nvim')
+  endif
+
+  call dein#end()
+  call dein#save_state()
+endif
+
+filetype plugin indent on
+syntax enable
+
+if dein#check_install()
+  call dein#install()
+endif
+
+" air-line
+let g:airline_powerline_fonts = 1
+
+if !exists('g:airline_symbols')
+    let g:airline_symbols = {}
+endif
+
+" unicode symbols
+let g:airline_left_sep = '»'
+let g:airline_left_sep = '▶'
+let g:airline_right_sep = '«'
+let g:airline_right_sep = '◀'
+let g:airline_symbols.linenr = '␊'
+let g:airline_symbols.linenr = '␤'
+let g:airline_symbols.linenr = '¶'
+let g:airline_symbols.branch = '⎇'
+let g:airline_symbols.paste = 'ρ'
+let g:airline_symbols.paste = 'Þ'
+let g:airline_symbols.paste = '∥'
+let g:airline_symbols.whitespace = 'Ξ'
+
+" airline symbols
+let g:airline_left_sep = ''
+let g:airline_left_alt_sep = ''
+let g:airline_right_sep = ''
+let g:airline_right_alt_sep = ''
+let g:airline_symbols.branch = ''
+let g:airline_symbols.readonly = ''
+let g:airline_symbols.linenr = ''
---
+++ setup.sh
@@ -0,0 +1,99 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install wget curl gcc git make xclip vim fonts-powerline zsh neovim tmux rename build-essential
+
+
+# Set up Git
+git config --global core.eol lf           # Git changes line-endings to Linux when needed
+git config --global core.autocrlf input   # Automatically convert line-endings to Linux on commit
+git config --global init.defaultBranch main  # Set default 'git init' branch name to 'main'
+
+
+# Set up Neovim
+mkdir -p ~/.config/nvim
+mv ./init.vim ~/.config/nvim/init.vim
+# dein plugin manager
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm ./installer.sh
+# Install plugins
+nvim -c 'call dein#install()'
+
+
+# zsh setup
+# Trigger zsh first time user setup.
+zsh -v
+# Set zsh as default terminal
+chsh -s /bin/zsh
+# Get directory colors
+cd ~ && curl -o ~/dircolors.256dark https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark && cd -
+# zsh config
+echo "PS2='> '"						                    | tee -a ~/.bashrc ~/.zshrc
+echo "alias c='clear'"		                    | tee -a ~/.bashrc ~/.zshrc
+echo "alias lsa='ls -a'"	                    | tee -a ~/.bashrc ~/.zshrc
+echo "alias t='tmux'"			                    | tee -a ~/.bashrc ~/.zshrc
+echo "alias t0='tmux attach-session -t 0'"    | tee -a ~/.bashrc ~/.zshrc
+echo "alias t1='tmux attach-session -t 1'"    | tee -a ~/.bashrc ~/.zshrc
+echo "alias ..='cd ..'"                       | tee -a ~/.bashrc ~/.zshrc
+echo "alias ...='cd ../..'"                   | tee -a ~/.bashrc ~/.zshrc
+echo "alias ....='cd ../../..'"               | tee -a ~/.bashrc ~/.zshrc
+echo "alias .....='cd ../../../..'"           | tee -a ~/.bashrc ~/.zshrc
+echo "eval \`dircolors ~/dircolors.256dark\`" | tee -a ~/.bashrc ~/.zshrc
+# Check if powerline-fonts for agnoster are installed correctly
+echo "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699"
+
+
+# tmux setup
+# oh-my-tmux
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+ln -s -f ~/.tmux/.tmux.conf -t ~
+cp ~/.tmux/.tmux.conf.local ~
+# tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+
+
+# Set up nodenv
+# Backup any existing copy first. Proceed only if it succeeds.
+sudo mv -i ~/.nodenv ~/.nodenv-backup && \
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+cd ~/.nodenv && src/configure && make -C src
+echo 'export PATH="$HOME/.nodenv/bin:$PATH"' | tee -a ~/.bashrc ~/.zshrc
+echo 'eval "$(nodenv init -)"' | tee -a ~/.bashrc ~/.zshrc
+
+# Close and all open terminals to load nodenv
+# to proceed past this point.
+
+# Install nodenv-build - to be able to install node versions
+mkdir -p "$(nodenv root)"/plugins
+git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+# Test if nodenv is set up correctly
+curl -fsSL https://github.com/nodenv/nodenv-installer/raw/master/bin/nodenv-doctor | bash
+
+git clone git@github.com:advaitju/linux-tools.git ~
+sh ~/linux-tools/setup.sh
+
+# Set up 
+## mkdir ~/scripts
+# mv ./s ./take ~/scripts/
+## mv ./s ~/scripts/
+## sudo chmod +x ~/scripts/*
+## ln -s ~/scripts/s /bin
+## ln -s ~/scripts/take /bin
+
+
+## Do manually
+## Set ZSH_THEME="agnoster"
+
+
+## Add github SSH keys
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/github_id_rsa
+# xclip -sel clip < ~/.ssh/github_id_rsa.pub
+## Test github connection
+# ssh -T git@github.com
