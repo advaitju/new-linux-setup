d43d0863b367d8160cc12eb9c3ea6b0b2a5be5b6
Advait Junnarkar <advaitju@users.noreply.github.com>
2022-11-08 10:40:12 +1100

    Update new-mac.sh

--- new-mac.sh
+++ new-mac.sh
@@ -31,7 +31,8 @@ echo "alias tls='tmux ls'\n" | tee -a ~/.zsh_aliases
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install wget tmux
+brew install wget tmux nvm
+brew install --cask visual-studio-code
 
 
 # tmux
