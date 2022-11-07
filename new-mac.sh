313318bf9a227e46ab97488bf7675891df24314c
Advait Junnarkar <advaitju@users.noreply.github.com>
2022-11-07 20:30:07 +1100

    Update new-mac.sh

--- new-mac.sh
+++ new-mac.sh
@@ -1,1 +1,26 @@
+#! /bin/bash
 
+# Git
+git config --global core.eol lf           # Git changes line-endings to Linux when needed
+git config --global core.autocrlf input   # Automatically convert line-endings to Linux on commit
+git config --global init.defaultBranch main  # Set default 'git init' branch name to 'main'
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install wget tmux
+
+# tmux
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+cp ~/.tmux/.tmux.conf ~
+rm -rf ~/.tmux
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+
+echo "set -g mouse on"                                                 | tee -a ~/.tmux.conf
+echo "set -g @plugin 'tmux-plugins/tmux-sensible'"                     | tee -a ~/.tmux.conf
+echo "set -g @plugin 'tmux-plugins/tmux-resurrect'"                    | tee -a ~/.tmux.conf
