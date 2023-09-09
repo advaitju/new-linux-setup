154fdbe1348b6ef7f154031478bad01c30a4bc20
Advait Junnarkar <advaitju@users.noreply.github.com>
2023-09-09 12:22:01 +1000

    Update new-mac.sh

--- new-mac.sh
+++ new-mac.sh
@@ -14,7 +14,7 @@ echo "alias c='clear && tput reset'" | tee -a ~/.zsh_aliases
 echo | tee -a ~/.zsh_aliases
 
 echo "alias ga='git add .'" | tee -a ~/.zsh_aliases
-echo "# alias gc='git commit -m'" | tee -a ~/.zsh_aliases
+echo "alias gc='git commit -m'" | tee -a ~/.zsh_aliases
 echo "# Fast git commit" | tee -a ~/.zsh_aliases
 echo "alias gcf='git add . && git commit -m \"Commit\" && git push'" | tee -a ~/.zsh_aliases
 echo "# git config (work)" | tee -a ~/.zsh_aliases
@@ -33,6 +33,8 @@ echo "alias t2='tmux attach-session -t 2'"     | tee -a ~/.zsh_aliases
 echo "alias t3='tmux attach-session -t 3'"     | tee -a ~/.zsh_aliases
 echo "alias tls='tmux ls'"                     | tee -a ~/.zsh_aliases
 echo "alias tr='tmux rename-session -t'\n"     | tee -a ~/.zsh_aliases
+
+echo "source ~/.zsh_aliases"                   | tee -a ~/.zshrc
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
