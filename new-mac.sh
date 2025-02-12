f32213cf435b8f91d63a1c6f02abfbb4e507437d
Advait Junnarkar <advaitju@users.noreply.github.com>
2025-02-12 16:25:03 +1100

    Update new-mac.sh

--- new-mac.sh
+++ new-mac.sh
@@ -17,8 +17,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 echo | tee -a ~/.zshrc
 echo | tee -a ~/.zshrc
-echo "alias c='clear && tput reset'" | tee -a ~/.zshrc
-echo "alias rm='rm -rf'"             | tee -a ~/.zshrc
+echo "alias c='clear && tput reset'"           | tee -a ~/.zshrc
+echo "alias rm='rm -rf'"                       | tee -a ~/.zshrc
 echo | tee -a ~/.zshrc
 
 echo "alias ga='git add .'"                    | tee -a ~/.zshrc
@@ -34,7 +34,7 @@ echo "alias gcfw='git config user.name \"Advait Junnarkar\" && git config user.e
 echo | tee -a ~/.zshrc
 
 echo "alias gl='git log'"                      | tee -a ~/.zshrc
-echo "alias gps='git push'"                     | tee -a ~/.zshrc
+echo "alias gps='git push'"                    | tee -a ~/.zshrc
 echo "alias gpl='git pull'"                    | tee -a ~/.zshrc
 echo "alias gs='git status'"                   | tee -a ~/.zshrc
 echo "alias gsp='git stash && git pull && git push && git stash pop'" | tee -a ~/.zshrc
@@ -45,6 +45,8 @@ echo | tee -a ~/.zshrc
 
 echo "alias n='npm'"                           | tee -a ~/.zshrc
 echo | tee -a ~/.zshrc
+
+echo "alias windsurf='open -a Windsurf'"        | tee -a ~/.zshrc
 
 
 # brew
@@ -59,7 +61,6 @@ brew install --cask --no-quarantine middleclick
 # brew - Work
 # brew - Personal
 brew install gh && gh auth login
-
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
